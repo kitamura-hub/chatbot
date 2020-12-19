68061f50cfe2ae0dce04b9dcf0933269130406d2
kitamura-hub <masato1999824@icloud.com>
2020-12-19 16:09:30 +0900

    change import App -> App.jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/styles/index.css';
-import App from './App';
+import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
