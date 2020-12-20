f34e94cfa4bd78364e82897cd66805e2432fb244
kitamura-hub <masato1999824@icloud.com>
2020-12-20 15:44:38 +0900

    Stored the dataset in state and passed it to the child component

--- src/App.jsx
+++ src/App.jsx
@@ -15,11 +15,26 @@ export default class App extends React.Component {
     }
   }
 
+  // datasetの中身をstateに格納する関数
+  initAnswer = () => {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const initAnswers = initDataset.answers;
+
+    this.setState({
+      answers: initAnswers
+    });
+  }
+
+  // 最初に描画されたときの処理
+  componentDidMount() {
+    this.initAnswer();
+  }
+
   render() {
     return (
       <sction className="c-section">
         <div className="c-box">
-          <AnswersList />
+          <AnswersList answers={this.state.answers} />
         </div>
       </sction>
     );
