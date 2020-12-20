db23c8ce44f9f5497b2b887fd76376f30363e343
kitamura-hub <masato1999824@icloud.com>
2020-12-20 15:00:42 +0900

    add Material UI Button

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+
+// Material UIのスタイルを決定する関数(ここでスタイルをカスタマイズできる)
+const useStyles = makeStyles((theme) => ({
+  root: {
+  },
+}));
 
 const Answer = () => {
+  // const classes = useStyles();
+  return(
+    <Button variant="contained">Default</Button>
+  );
+
 }
 export default Answer;
