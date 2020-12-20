71857a9ad7ce85cbfe7ced8f8866ac6d332e68ed
kitamura-hub <masato1999824@icloud.com>
2020-12-20 18:17:58 +0900

    Create a template for the chat part

---
+++ src/components/Chat.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+
+const Chat = (props) => {
+  const isQuestion = (props.type === 'question');  // 質問かどうか
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse';
+
+  return(
+    <ListItem className={classes}>
+      <ListItemAvatar>
+        <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{ props.text }</div>
+    </ListItem>
+  );
+}
+export default Chat;
