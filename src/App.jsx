5dc3e9ce797adc66ed87438cd94d33d72fe85070
kitamura-hub <masato1999824@icloud.com>
2020-12-23 16:16:13 +0900

    Implemented chat delay to init question

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default class App extends React.Component {
   selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
       case (nextQuestionId === "init"):
-        this.displayNextQuestion(nextQuestionId);
+        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
         break;
       default :
         const chats = this.state.chats;
