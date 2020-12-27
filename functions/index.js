d2e1bd27d96246f39a636fbd650113f710269dd8
kitamura-hub <masato1999824@icloud.com>
2020-12-27 11:26:30 +0900

    add: Create api for https function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
-// const functions = require('firebase-functions');
+import * as functions from 'firebase-functions';
+import * as admin from "firebase-admin";
+admin.initializeApp();
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const sendResponse = (response, statusCode, body) => {
+  response.send({
+    statusCode,
+    body: JSON.stringify(body)
+  });
+};
+
+export const addDataset = functions.https.onRequest(async (req, res) => {
+  if (req.method !== 'POST') {
+    sendResponse(res, 405, {error: "Invalid Request"})
+  } else {
+    const dataset = req.body;
+    for (const key of Object.keys(dataset)) {
+      const data = dataset[key];
+      await db.collection('questions').doc(key).set(data)
+    }
+    sendResponse(res, 200, {message: 'Successfully added dataset! WooHoo!'});
+  }
+});
