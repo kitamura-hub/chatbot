7305f6999d4c66fec8113b4aeef749d86367c9a3
kitamura-hub <masato1999824@icloud.com>
2020-12-28 13:00:13 +0900

    refactor: replace React hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './assets/styles/style.css'
 import defaultDataset from './dataset';
 import { AnswersList, Chats } from './components/index.js';
 import FormDialog from './components/Forms/FormDialog';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      answers: [],  // 回答コンポーネントに表示するデータ
-      chats: [],  // チャットコンポーネントに表示するデータ
-      currentId: "init",  // 現在の質問ID
-      dataset: defaultDataset,  // 質問と回答のデータセット
-      open: false,  // お問い合わせフォーム用のモーダルの開閉を管理
-    }
-    this.selectedAnswer = this.selectAnswer.bind(this);  // コンポーネントが描画される度にselectAnswer関数が生成されるのを防ぐためにbindを行う
-    this.handleClose = this.handleClose.bind(this);
-    this.handleClickOpen = this.handleClickOpen.bind(this);
-  }
+const App = () => {
+  const [answers, setAnswers] = useState([]);               // 回答コンポーネントに表示するデータ
+  const [chats, setChats] = useState([]);                   // チャットコンポーネントに表示するデータ
+  const [currentId, setCurrentId] = useState("init");       // 現在の質問ID
+  const [dataset] = useState(defaultDataset);               // 質問と回答のデータセット
+  const [open, setOpen] = useState(false);                  // お問い合わせフォーム用のモーダルの開閉を管理
 
   /**
    * 次の質問を表示する関数
    * @param { string } nextQuestionId 質問文を特定する一意のid
+   * @param { string } nextDataset nextQuestionIdをキーに持つvalue
    */
-  displayNextQuestion = (nextQuestionId) => {
-    const chats = this.state.chats;
-    chats.push({
-      text: this.state.dataset[nextQuestionId].question,
+  const displayNextQuestion = (nextQuestionId, nextDataset) => {
+    addChats({
+      text: nextDataset.question,
       type: 'question'
     })
-    // 格stateの更新(ここで画面の状態を更新)
-    this.setState({
-      answers: this.state.dataset[nextQuestionId].answers,
-      chats: chats,
-      currentId: nextQuestionId,
-    })
+    // stateの更新(ここで画面の状態を更新)
+    setAnswers(nextDataset.answers);
+    setCurrentId(nextQuestionId);
   }
 
   /**
@@ -42,14 +31,10 @@ export default class App extends React.Component {
    * @param { string } selectedAnswer 回答文
    * @param { string } nextQuestionId 質問文を特定する一意のid
    */
-  selectAnswer = (selectedAnswer, nextQuestionId) => {
+  const selectAnswer = (selectedAnswer, nextQuestionId) => {
     switch(true) {
-      case (nextQuestionId === "init"):
-        setTimeout(() => this.displayNextQuestion(nextQuestionId), 500);
-        break;
-
       case (nextQuestionId === "contact"):
-        this.handleClickOpen();
+        handleClickOpen();
         break;
 
       case (/^https:*/.test(nextQuestionId)):
@@ -60,51 +45,50 @@ export default class App extends React.Component {
         break;
 
       default :
-        // chatsの更新
-        const chats = this.state.chats;
-        chats.push({
+        addChats({
           text: selectedAnswer,
           type: 'answer'
         })
-        this.setState({
-          chats: chats
-        });
-
-        setTimeout(() => this.displayNextQuestion(nextQuestionId) , 1000);
+        setTimeout(() => displayNextQuestion(nextQuestionId, dataset[nextQuestionId]) , 1000);
         break;
     }
   }
 
-  handleClickOpen = () => {
-    this.setState({open: true});
-  };
-  handleClose = () => {
-    this.setState({open: false});
-  };
-
-  // 最初に描画されたときの処理
-  componentDidMount() {
-    const initAnswer = "";
-    this.selectAnswer(initAnswer, this.state.currentId);
+  // chatsを更新する関数
+  const addChats = (chat) => {
+    setChats(prevChats => {
+      return [...prevChats, chat];  // prevChatsは前回のchats(state)の状態
+    })
   }
 
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, [setOpen]);
+
+  // 最初に描画されたときの処理
+  useEffect(() => {
+    displayNextQuestion(currentId, dataset[currentId]);
+  }, []);  //初回のみ実行して欲しいため、空の配列を渡す
+
   // 最新のチャットが見えるように、スクロール位置の頂点をスクロール領域の最下部に設定
-  componentDidUpdate() {
+  useEffect(() => {
     const scrollArea = document.getElementById("scroll-area");
     if (scrollArea) {  // scrollAreaが存在していれば
       scrollArea.scrollTop = scrollArea.scrollHeight;
     }
-  }
+  });
 
-  render() {
-    return (
-      <sction className="c-section">
-        <div className="c-box">
-          <Chats chats={this.state.chats} />
-          <AnswersList answers={this.state.answers} select={this.selectAnswer} />
-          <FormDialog open={this.state.open} handleClose={this.handleClose} />
-        </div>
-      </sction>
-    );
-  }
+  return (
+    <sction className="c-section">
+      <div className="c-box">
+        <Chats chats={chats} />
+        <AnswersList answers={answers} select={selectAnswer} />
+        <FormDialog open={open} handleClose={handleClose} />
+      </div>
+    </sction>
+  );
 }
+export default App;
