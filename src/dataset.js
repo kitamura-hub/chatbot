83a9c7ed5f72906001888e0ad749435507535e4b
kitamura-hub <masato1999824@icloud.com>
2020-12-28 17:03:59 +0900

    changed dataset

--- src/dataset.js
+++ src/dataset.js
@@ -1,19 +1,19 @@
 const dataset = {
   "init": {
       answers: [
-          {content: "仕事を依頼したい", nextId: "job_offer"},
-          {content: "エンジニアのキャリアについて相談したい", nextId: "consultant"},
-          {content: "学習コミュニティについて知りたい", nextId: "community"},
-          {content: "お付き合いしたい", nextId: "dating"}
+          {content: "仕事を依頼したい", nextId: "job"},
+          {content: "好きな言語は何ですか？", nextId: "language"},
+          {content: "一緒に開発してみたい", nextId: "development"},
+          {content: "お喋りしたい", nextId: "chating"}
       ],
-      question: "こんにちは！🐯トラハックへのご用件はなんでしょうか？"
+      question: "こんにちは！ご用件はなんでしょうか？"
   },
-  "job_offer": {
+  "job": {
       answers: [
           {content: "Webサイトを制作してほしい", nextId: "website"},
           {content: "Webアプリを開発してほしい", nextId: "webapp"},
           {content: "自動化ツールを作ってほしい", nextId: "automation_tool"},
-          {content: "その他", nextId: "other_jobs"}
+          {content: "その他", nextId: "other"}
       ],
       question: "どのようなお仕事でしょうか？"
   },
@@ -38,40 +38,147 @@ const dataset = {
       ],
       question: "自動化ツール開発についてですね。コチラからお問い合わせできます。"
   },
-  "other_jobs": {
+  "other": {
       answers: [
           {content: "問い合わせる", nextId: "contact"},
           {content: "最初の質問に戻る", nextId: "init"}
       ],
       question: "その他についてですね。コチラからお問い合わせできます。"
   },
-  "consultant": {
+  "language": {
       answers: [
-          {content: "YouTubeで動画を見る", nextId: "https://www.youtube.com/channel/UC-bOAxx-YOsviSmqh8COR0w"},
-          {content: "学習コミュニティについて知りたい", nextId: "community"},
+          {content: "JavaScript", nextId: "JavaScript"},
+          {content: "Ruby", nextId: "Ruby"},
+          {content: "PHP", nextId: "PHP"},
+          {content: "Java", nextId: "Java"}
+      ],
+      question: "この中からお好きな言語をお選びください"
+  },
+  "JavaScript": {
+      answers: [
+          {content: "React.js", nextId: "React"},
+          {content: "Vue.js", nextId: "Vue"},
+          {content: "Angular.js", nextId: "Angular"},
           {content: "最初の質問に戻る", nextId: "init"}
       ],
-      question: "トラハックは普段からYouTubeでキャリアについて発信しています。また、僕が運営するエンジニア向け学習コミュニティ内でも相談に乗っていますよ。"
+      question: "JavaScript良いですよね！私も好きです😊良ければ、お好きのライブラリやFWも教えてください！"
   },
-  "community": {
+  "Ruby": {
       answers: [
-          {content: "どんな活動をしているの？", nextId: "community_activity"},
-          {content: "コミュニティに参加したい", nextId: "https://torahack.web.app/community/"},
+          {content: "Ruby on Rails", nextId: "RoR"},
+          {content: "Sinatra", nextId: "Sinatra"},
+          {content: "HANAMI", nextId: "HANAMI"},
           {content: "最初の質問に戻る", nextId: "init"}
       ],
-      question: "2020年3月から学習コミュニティを始めました！🎉Webエンジニアへの転職を目指す人向けに、プログラミングを教えたりキャリアの相談に乗っています。"
+      question: "Ruby良いですね！私は使ったことがありませんが、楽しそうです😊良ければ、お好きのライブラリやFWも教えてください！"
   },
-  "community_activity": {
+  "PHP": {
       answers: [
-          {content: "さらに詳細を知りたい", nextId: "https://youtu.be/tIzE7hUDbBM"},
-          {content: "コミュニティに参加したい", nextId: "https://torahack.web.app/community/"},
+          {content: "Larabel", nextId: "Larabel"},
+          {content: "CakePHP", nextId: "CakePHP"},
+          {content: "Symfony", nextId: "Symfony"},
           {content: "最初の質問に戻る", nextId: "init"}
       ],
-      question: "フロントエンド向けの教材の提供、キャリアや勉強法に関するメルマガの配信、週1のオンライン作業会などを開催しています！\n詳細はYouTube動画で紹介しています。"
+      question: "PHP良いですね！私も少しだけ学習していました！😊良ければ、お好きのライブラリやFWも教えてください！"
   },
-  "dating": {
+  "Java": {
       answers: [
-          {content: "DMする", nextId: "https://twitter.com/torahack_"},
+          {content: "Spring", nextId: "Spring"},
+          {content: "Spring Boot", nextId: "Spring Boot"},
+          {content: "Play", nextId: "Play"},
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Java良いですね！私も少しだけ学習していました！😊良ければ、お好きのライブラリやFWも教えてください！"
+  },
+
+  "React": {
+      answers: [
+          {content: "DMする", nextId: "https://twitter.com/masa99_it"},
+          {content: "Githubを見る", nextId: "https://github.com/kitamura-hub"},
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "React良いですね！私の大好きなライブラリです！😊貴方とは気が合いそうなので、良ければ私のSNSもチェックしてみてください！"
+  },
+  "Vue": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Vue良いですね！😊楽しいチャットをありがとうございました！"
+  },
+  "Angular": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Angular良いですね！😊楽しいチャットをありがとうございました！"
+  },
+
+  "RoR": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Ruby on Rails良いですね！😊楽しいチャットをありがとうございました！"
+  },
+  "Sinatra": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "失礼ながら、Sinatraは存じ上げません。ですが、貴方のお気に入りを教えて頂き嬉しく思います！"
+  },
+  "HANAMI": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "失礼ながら、HANAMIは存じ上げません。ですが、貴方のお気に入りを教えて頂き嬉しく思います！"
+  },
+
+  "Larabel": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Larabel良いですね！私もPHPを学習していたときに使っていました！😊楽しいチャットをありがとうございました！"
+  },
+  "CakePHP": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "CakePHPは聞いたことがある程度ですね..楽しいチャットをありがとうございました！"
+  },
+  "Symfony": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Symfonyは聞いたことがある程度ですね..楽しいチャットをありがとうございました！"
+  },
+
+  "Spring": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Springは聞いたことがある程度ですね..楽しいチャットをありがとうございました！"
+  },
+  "Spring Boot": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Spring Bootは聞いたことがある程度ですね..楽しいチャットをありがとうございました！"
+  },
+  "Play": {
+      answers: [
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "Playは聞いたことがある程度ですね..楽しいチャットをありがとうございました！"
+  },
+
+  "development": {
+      answers: [
+          {content: "Githubを見る", nextId: "https://github.com/kitamura-hub"},
+          {content: "最初の質問に戻る", nextId: "init"}
+      ],
+      question: "私と一緒に開発しませんか？😊"
+  },
+  "chating": {
+      answers: [
+          {content: "DMする", nextId: "https://twitter.com/masa99_it"},
           {content: "最初の質問に戻る", nextId: "init"}
       ],
       question: "まずは一緒にランチでもいかがですか？DMしてください😘"
